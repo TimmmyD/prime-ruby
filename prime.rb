840666b71c5762e614b37f33a451307286dd0943
TimmmyD <tmd112593@gmail.com>
2020-10-29 17:02:09 -0700

    commit tree 52b542f3e4b79bed6a8b9874e8a54b24692a13e7

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+    if number <= 1
+        return false
+    else
+        (2...number).to_a.each do |num|
+            if number % num == 0
+                return false
+            end
+        end
+        return true
+    end
+end
